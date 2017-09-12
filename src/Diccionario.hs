ff4f07b97aacbbad434fe8285896242c17b50c09
Gustavo Ajzenman <gustavoajz@gmail.com>
2017-09-11 21:12:13 -0300

    Linting

--- src/Diccionario.hs
+++ src/Diccionario.hs
@@ -20,20 +20,20 @@ import Arbol23
 
 {- Definiciones de tipos. -}
 
-type Comp clave = clave->clave->Bool
-type Estr clave valor = Arbol23 (clave,valor) clave
+type Comp clave = clave -> clave -> Bool
+type Estr clave valor = Arbol23 (clave, valor) clave
 
-data Diccionario clave valor = Dicc {cmp :: Comp clave, estructura :: Maybe (Estr clave valor)}
+data Diccionario clave valor = Dicc { cmp :: Comp clave, estructura :: Maybe (Estr clave valor) }
 --El comparador es por menor.
 
 {- Funciones provistas por la cátedra. -}
 
 --Inserta un nuevo par clave, valor en una estructura que ya tiene al menos un dato.
-insertar::clave->valor->Comp clave->Estr clave valor-> Estr clave valor
+insertar :: clave -> valor -> Comp clave -> Estr clave valor -> Estr clave valor
 insertar c v comp a23 = interceptar (insertarYPropagar c v comp a23) id (\s1 (c1, s2)->Dos c1 s1 s2)
 
 --Maneja el caso de que la segunda componente sea Nothing.
-interceptar::(a,Maybe b)->(a->c)->(a->b->c)->c
+interceptar :: (a, Maybe b) -> (a -> c) -> (a -> b -> c) -> c
 interceptar (x,y) f1 f2 = case y of
                    Nothing -> f1 x
                    Just z -> f2 x z
@@ -41,7 +41,7 @@ interceptar (x,y) f1 f2 = case y of
 {- Inserta una clave con su valor correspondiente. Si se actualiza el índice, el cambio se propaga hacia arriba
    para mantener balanceado el árbol.
    Usamos recursión explícita porque este tipo de recursión no es estructural (no se aplica a todos los hijos). -}
-insertarYPropagar::clave->valor->Comp clave->Estr clave valor-> (Estr clave valor, Maybe (clave, Estr clave valor))
+insertarYPropagar :: clave -> valor -> Comp clave -> Estr clave valor -> (Estr clave valor, Maybe (clave, Estr clave valor))
 insertarYPropagar c v comp a23 = let rec = insertarYPropagar c v comp in case a23 of
     --Si es hoja, elegimos la máxima de las claves y propagamos el balanceo hacia arriba.
     Hoja (ch,vh) -> if comp c ch 
@@ -81,12 +81,12 @@ insertarYPropagar c v comp a23 = let rec = insertarYPropagar c v comp in case a2
                         (\s1 (c3, s2) -> (Dos c1 a1 a2, Just(c2, Dos c3 s1 s2)))
 
 --Se asume que la lista no tiene claves repetidas.
-definirVarias::[(clave,valor)]->Diccionario clave valor->Diccionario clave valor
+definirVarias :: [(clave,valor)] -> Diccionario clave valor -> Diccionario clave valor
 definirVarias = (flip.foldr.uncurry) definir
 
 {- Funciones a implementar. -}
 
-vacio :: Comp clave->Diccionario clave valor
+vacio :: Comp clave -> Diccionario clave valor
 vacio comparador = Dicc {cmp = comparador, estructura = Nothing}
 
 
@@ -98,10 +98,8 @@ definir clave valor dicc = Dicc
         insertar' (Just estr) = insertar clave valor (cmp dicc) estr
         insertar' Nothing = Hoja (clave, valor)
         
-        -- Dicc { cmp = (cmp dicc)
-    -- , estructura = insertar clave valor (cmp dicc) (estructura dicc) }
 
-obtener:: Eq clave =>clave->Diccionario clave valor->Maybe valor
+obtener :: Eq clave => clave -> Diccionario clave valor -> Maybe valor
 obtener clave dicc = obtener' (estructura dicc)
     where 
         obtener' (Just estr) = dameValor clave (cmp dicc) estr
@@ -117,7 +115,7 @@ dameValor cl comp arbol = foldA23 f1 f2 f3 arbol
 
 
 
-claves::Diccionario clave valor->[clave]
+claves :: Diccionario clave valor -> [clave]
 claves dicc = fromMaybe [] (fmap (map fst . hojas) (estructura dicc))
 
 
